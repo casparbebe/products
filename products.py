7b6c21c693134e2efa638feb293a33f1adbac6c7
Caspar Chang <caspar2201@gmail.com>
2021-06-10 10:42:22 +0800

    add read file

--- products.py
+++ products.py
@@ -1,24 +1,28 @@
+# 讀取檔案
 products = []
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	price = int(price)
-	products.append([name, price])
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 跳到下回合
+		name, price = line.strip().split(',')
+		products.append([name, price])
 print(products)
 
-# 簡潔的寫法 products.append([name, price])
-# p = []
-	# p.append(name)
-	# p.append(price)
-	# products.append(p)
+# 讓使用者輸入
+while True:
+	name = input('請輸入商品名稱: ') # 簡潔的寫法 products.append([name, price])
+	if name == 'q':                 # p = []
+		break                       # p.append(name)
+	price = int(price)              # p.append(price)
+	products.append([name, price])  # products.append(p)	
+print(products)
 
+# 印出所有購買紀錄
 for product in products:
 	print(product[0], '的價格是', product[1])
 
-
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '元' + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
